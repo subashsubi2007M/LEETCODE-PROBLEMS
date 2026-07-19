ba6cd2f6d82d772e4e8d161158fac82f94f2af9d
subash <subashsubi2007@gmail.com>
2026-07-19 22:21:28 +0530

    1114.Print in Order.java

---
+++ 1114.Print in Order.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.Semaphore;
+
+class Foo {
+    
+    private Semaphore sem2;
+    private Semaphore sem3;
+
+    public Foo() {
+        
+        sem2 = new Semaphore(0);
+        sem3 = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        
+        printFirst.run();
+        
+        sem2.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        
+        sem2.acquire();
+        
+        printSecond.run();
+        
+        sem3.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        
+        sem3.acquire();
+        
+        printThird.run();
+    }
+}
