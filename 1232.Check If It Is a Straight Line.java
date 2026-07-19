312a7c2b4465c8a64dec923d962e8e12d22c9746
subash <subashsubi2007@gmail.com>
2026-07-19 21:27:02 +0530

    1232.Check If It Is a Straight Line.java

---
+++ 1232.Check If It Is a Straight Line.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkStraightLine(int[][] coordinates) {
+       
+        if (coordinates.length == 2) {
+            return true;
+        }
+        
+        
+        int x0 = coordinates[0][0], y0 = coordinates[0][1];
+        int x1 = coordinates[1][0], y1 = coordinates[1][1];
+        
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+         
+        
+        for (int i = 2; i < coordinates.length; i++) {
+            int xi = coordinates[i][0];
+            int yi = coordinates[i][1];
+            
+            
+            if (dy * (xi - x0) != dx * (yi - y0)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
