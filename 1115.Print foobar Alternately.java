ed224ab1a8b5e58398c1cbdaef2d10963751921a
subash <subashsubi2007@gmail.com>
2026-07-19 22:22:55 +0530

    1115.Print foobar Alternately.java

---
+++ 1115.Print foobar Alternately.java
@@ -0,0 +1,32 @@
+import java.util.concurrent.Semaphore;
+
+class FooBar {
+    private int n;
+
+    
+    private Semaphore fooSemaphore = new Semaphore(1);
+    
+    private Semaphore barSemaphore = new Semaphore(0);
+
+    public FooBar(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            fooSemaphore.acquire();   
+            printFoo.run();           
+            barSemaphore.release();   
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            barSemaphore.acquire();   
+            printBar.run();           
+            fooSemaphore.release();   
+        }
+    }
+}
